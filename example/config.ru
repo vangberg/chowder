06c5d53605a0336f223626ca562ce3323fb8ab09
Harry Vangberg <harry@vangberg.name>
2009-02-26 09:14:09 +0100

    openid!!

--- example/config.ru
+++ example/config.ru
@@ -1,8 +1,13 @@
+$:.unshift(File.join(File.dirname(File.expand_path(__FILE__)), '..', 'lib'))
+$:.unshift(File.dirname(File.expand_path(__FILE__)))
 require 'chowder'
 require 'app'
 
 use Rack::Session::Cookie
-use Chowder do |login, password|
+use Chowder::Basic do|login, password|
   login == "harry" && password == "clamchowder"
+end
+use Chowder::OpenID do |url|
+  url == 'http://harry.vangberg.name/' ? url : nil
 end
 run Sinatra::Application
--- lib/chowder.rb
+++ lib/chowder.rb
@@ -1,50 +1,88 @@
 require 'sinatra/base'
 require 'ostruct'
+require 'openid'
+require 'openid/store/filesystem'
 
-class Chowder < Sinatra::Base
-  # Override this until in Sinatra supports it. See
-  # http://sinatra.lighthouseapp.com/projects/9779/tickets/160
-  def initialize(app=nil, *args, &block)
-    @app = app
-    @middleware = OpenStruct.new(:args => args, :block => block)
-  end
-
-  helpers do
-    def find_login_template
-      views_dir = self.options.views || "./views"
-      template = Dir[File.join(views_dir, 'login.*')].first
-    end
-  end
-
-  get '/login' do
-    if template = find_login_template
-      engine = File.extname(template)[1..-1]
-      # Resort to this dirty hack. Haml/erb are required by haml()/erb() and
-      # thus calling render() without requiring haml/erb will result in error
-      # and this is just shorter bla bla bla
-      send(engine, :login)
-    else
-      <<-HTML
+module Chowder
+  class Base < Sinatra::Base
+    LOGIN_VIEW = <<-HTML
       <form action="/login" method="POST">
         Login: <input type="text" name="login" /><br />
         Password: <input type="password" name="password" /><br />
         <input type="submit" value="Login" />
       </form>
-      HTML
+      OpenID:
+      <form action="/openid/initiate" method="POST">
+        URL: <input type="text" name="openid_identifier" /><br />
+        <input type="submit" value="Login" />
+      </form>
+    HTML
+
+    # Override this until in Sinatra supports it. See
+    # http://sinatra.lighthouseapp.com/projects/9779/tickets/160
+    def initialize(app=nil, *args, &block)
+      @app = app
+      @middleware = OpenStruct.new(:args => args, :block => block)
+    end
+
+    def find_login_template
+      views_dir = self.options.views || "./views"
+      template = Dir[File.join(views_dir, 'login.*')].first
+    end
+
+    get '/login' do
+      if template = find_login_template
+        engine = File.extname(template)[1..-1]
+        send(engine, :login)
+      else
+        LOGIN_VIEW
+      end
+    end
+
+    get '/logout' do
+      session[:current_user] = nil
+      redirect '/'
     end
   end
 
-  post '/login' do
-    login, password = params[:login], params[:password]
-    if session[:current_user] = @middleware.block.call(login, password)
-      redirect(session[:redirect_to] || '/')
-    else
+  class Basic < Base
+    post '/login' do
+      login, password = params[:login], params[:password]
+      if session[:current_user] = @middleware.block.call(login, password)
+        redirect(session[:redirect_to] || '/')
+      else
+        redirect '/login'
+      end
+    end
+  end
+
+  class OpenID < Base
+    def host
+      host = env['HTTP_HOST'] || "#{env['SERVER_NAME']}:#{env['SERVER_PORT']}"
+      "http://#{host}"
+    end
+
+    def setup_consumer
+      store = ::OpenID::Store::Filesystem.new('.openid')
+      osession = session[:openid] ||= {}
+      @consumer = ::OpenID::Consumer.new(osession, store)
+    end
+
+    post '/openid/initiate' do
+      setup_consumer
+      url = @consumer.begin(params['openid_identifier']).redirect_url(host, host + '/openid/authenticate')
+      redirect url
+    end
+
+    get '/openid/authenticate' do
+      setup_consumer
+      res = @consumer.complete(request.params, host + '/openid/authenticate')
+      if res.is_a?(::OpenID::Consumer::SuccessResponse)
+        if session[:current_user] = @middleware.block.call(res.identity_url)
+          redirect(session[:redirect_to] || '/')
+        end
+      end
       redirect '/login'
     end
-  end
-
-  get '/logout' do
-    session[:current_user] = nil
-    redirect '/'
   end
 end
